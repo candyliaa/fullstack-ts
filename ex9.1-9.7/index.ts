34d60c05579a6f4c90b363382d3b4283a84880be
candyliaa <astronomy.dev@gmail.com>
2025-12-27 22:08:55 +0200

    ex.9.4

---
+++ ex9.1-9.7/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/ping', (_req: Request, res: Response) => {
+  res.send('pong');
+});
+
+app.get('/hello', (_req: Request, res: Response) => {
+  res.send('Hello Full Stack!');
+})
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
