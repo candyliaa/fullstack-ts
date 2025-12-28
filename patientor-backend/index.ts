36095dc1eefa1f9959d506432dd9327e420f9657
candyliaa <astronomy.dev@gmail.com>
2025-12-28 17:42:39 +0200

    change backend port, ping works; ex.9.9

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -2,7 +2,7 @@ import express, { Response, Request } from 'express';
 const app = express()
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req: Request, res: Response) => {
     res.send('pong');
