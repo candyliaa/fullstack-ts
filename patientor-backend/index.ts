3ef54262cc12c58a8d61fac0c723cfe3df274fab
candyliaa <astronomy.dev@gmail.com>
2025-12-28 17:29:47 +0200

    ex.9.8

---
+++ patientor-backend/index.ts
@@ -0,0 +1,13 @@
+import express, { Response, Request } from 'express';
+const app = express()
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/api/ping', (_req: Request, res: Response) => {
+    res.send('pong');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
