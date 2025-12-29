ea84288c105b1fc87eb174a8b52661304f617097
candyliaa <astronomy.dev@gmail.com>
2025-12-29 21:58:53 +0200

    ex.9.14

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { NonSensitivePatient } from '../types';
 import patientService from '../services/patientService';
 import toNewPatient from '../utils/utils';
+import z from 'zod';
 
 const router = express.Router();
 
@@ -15,12 +16,13 @@ router.post('/', (req: Request, res: Response) => {
 
         const addedPatient = patientService.addPatient(newPatient);
         res.json(addedPatient);
+
     } catch (error: unknown) {
-        let errorMessage = 'Something went wrong!';
-        if (error instanceof Error) {
-            errorMessage += ' Error: ' + error.message;
+        if (error instanceof z.ZodError) {
+            res.status(400).send({ error: error.issues });
+        } else {
+            res.status(400).send({ error: 'unknown error' });
         }
-        res.status(400).send(errorMessage);
     }
 });
 
