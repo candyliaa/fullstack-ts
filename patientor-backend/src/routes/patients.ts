3ef7c096d51df4a38ebcc51295cd780d4445b4f0
candyliaa <astronomy.dev@gmail.com>
2025-12-29 20:40:06 +0200

    ex.9.12

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -8,4 +8,16 @@ router.get('/', (_req: Request, res: Response<NonSensitivePatient[]>) => {
     res.send(patientService.getEntries());
 });
 
+router.post('/', (req: Request, res: Response) => {
+    const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+    const addedPatient = patientService.addPatient({
+        name,
+        dateOfBirth,
+        ssn,
+        gender,
+        occupation,
+    });
+    res.json(addedPatient);
+});
+
 export default router;
--- patientor-backend/src/services/patientService.ts
+++ patientor-backend/src/services/patientService.ts
@@ -1,5 +1,8 @@
 import patientData from '../../data/patients.ts';
-import { Patient, NonSensitivePatient } from '../types.ts'
+import { Patient, NonSensitivePatient, NewPatientEntry } from '../types.ts'
+import { v1 as uuid } from 'uuid';
+
+const id = uuid();
 
 const patients: Patient[] = patientData;
 
@@ -7,8 +10,14 @@ const getEntries = (): NonSensitivePatient[] => {
     return patients.map(({ ssn, ...rest }) => rest);
 };
 
-const addPatient = () => {
-    return null;
+const addPatient = (entry: NewPatientEntry): Patient => {
+    const newPatient = {
+        id,
+        ...entry
+    };
+
+    patients.push(newPatient);
+    return newPatient;
 };
 
 export default {
