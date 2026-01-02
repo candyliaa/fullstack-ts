b7508a81ee60de0720a37798cbf740746e16ba84
candyliaa <astronomy.dev@gmail.com>
2026-01-02 15:53:04 +0200

    ex.9.15

---
+++ ex9.15-9.20/ts-practice/src/Content.tsx
@@ -0,0 +1,27 @@
+interface CoursePart {
+    name: string;
+    exerciseCount: number;
+}
+
+interface courseParts {
+    courseParts: CoursePart[];
+}
+
+const Content = (props: courseParts) => {
+    return (
+    <>
+      <p>
+        {props.courseParts[0].name} {props.courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[2].name} {props.courseParts[2].exerciseCount}
+      </p>
+      <p></p>
+    </>
+    )
+}
+
+export default Content;
